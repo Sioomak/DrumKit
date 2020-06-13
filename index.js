b1b8d86b548071759b3bede28e57b8494fbbe2f9
Sioomak <66186918+Sioomak@users.noreply.github.com>
2020-06-13 17:22:55 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,133 @@
+
+// detecting Button Press
+
+var numberOFDrums = document.querySelectorAll(".drum").length;
+
+
+for (i = 0; i < numberOFDrums; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+    var buttonInnethtml = this.innerHTML;
+
+    makeSound(buttonInnethtml);
+    animation(buttonInnethtml);
+
+
+  });
+
+}
+
+// Detecting Keyboard Press
+
+document.addEventListener("keypress", function(event) {
+  
+    makeSound(event.key);
+    animation(event.key);
+
+});
+
+
+function makeSound(key){
+
+  switch (key) {
+
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "l":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    default:
+      console.log(buttonInnethtml);
+
+  }
+
+};
+
+
+function animation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+  
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100)
+}
+
+
+
+
+
+
+ 
+
+
+
+
+// /// adding event listener to the keys
+
+// document.addEventListener('keypress', (event) => {
+//   if (event.key == 'w') {
+//     var tom1 = new Audio("sounds/tom-1.mp3");
+//         tom1.play();
+//   }
+//   if (event.key == 'a') {
+//     var tom2 = new Audio("sounds/tom-2.mp3");
+//         tom2.play();
+//   }
+//   if (event.key == 's') {
+//     var tom3 = new Audio("sounds/tom-3.mp3");
+//         tom3.play();
+//   }
+//   if (event.key == 'd') {
+//     var tom4 = new Audio("sounds/tom-4.mp3");
+//         tom4.play();
+//   }
+//   if (event.key == 'j') {
+//     var snare = new Audio("sounds/snare.mp3");
+//         snare.play();
+//   }
+//   if (event.key == 'k') {
+//     var kick = new Audio("sounds/kick-bass.mp3");
+//         kick.play();
+//   }
+//   if (event.key == 'l') {
+//     var crash = new Audio("sounds/crash.mp3");
+//         crash.play();
+//   }
+// });
+  
+
+
